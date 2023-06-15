532db727ec620338aed3f4485eff26185cfbcca3
masi <masi.kun@live.de>
2023-06-15 08:32:38 +0200

    init new

--- titanic_data_evaluation.py
+++ titanic_data_evaluation.py
@@ -3,3 +3,9 @@ import spicy
 import numpy as np
 import matplotlib.pyplot as plt
 
+csv = pd.read_csv("TitanicData.csv")
+# print(csv)
+csv["Survived"].replace(to_replace=0, value=False, inplace=True)
+csv["Survived"].replace(to_replace=1, value=True, inplace=True)
+survivors = csv.groupby("Survived").agg({"PassengerId": "count"})
+print(survivors)

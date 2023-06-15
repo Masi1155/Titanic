5784976ed4d7d32e554e4b2a2846c8019400ef07
masi <masi.kun@live.de>
2023-06-15 11:20:02 +0200

    fixed Split

--- titanic_data_evaluation.py
+++ titanic_data_evaluation.py
@@ -11,11 +11,17 @@ csv["Survived"].replace(to_replace=0, value=False, inplace=True)
 csv["Survived"].replace(to_replace=1, value=True, inplace=True)
 survivors = csv.groupby("Survived").agg({"PassengerId": "count"})
 
-# csv[['Title', 'Name']] = csv['Name'].str.split('.', 1, expand=True)
+# csv['Title'], csv['Name'] = csv['Name'].apply(lambda x: x.split("."))[1][0].split(",")
+# print(csv['Title'])
+# csv['Surname'], csv['Title'] = csv['Title'].apply(lambda x: x.split(","))
+# print(temp[1][0])
+csv['Title'] = csv['Name'].str.split('.', expand=True)[0].str.split(',', expand=True)[1]
+csv['Surname'] = csv['Name'].str.split('.', expand=True)[0].str.split(',', expand=True)[0]
+csv['Name'] = csv['Name'].str.split('.', expand=True)[1]
 # csv[['Surname', 'Title']] = csv['Title'].str.split(',', 1, expand=True)
-#
+
 # title = csv.groupby('Title').describe()
-# print(csv[['Title', 'Name']])
+print(csv[['Title', 'Surname', 'Name']])
 # print(title)
 
 survive_fare_survived = csv.groupby('Fare').apply(lambda x: pd.Series((x['Survived']).sum(), index=['Überlebt']))
@@ -31,4 +37,4 @@ axes[0, 1].set_title("Überlebt")
 survive_fare_died.plot(style=".", ax=axes[0, 0], legend=False, color="c", xlim=(-10, 520), ylim=(-1, 30))
 axes[0, 0].set_title("Gestorben")
 
-plt.show()
+# plt.show()

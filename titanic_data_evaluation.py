ed493d6e693a46e29980dcb456ad7ab87a24ec21
KlausiDerBoy <123schulevorbei@googlemail.com>
2023-06-15 08:52:41 +0200

    changed Title

--- titanic_data_evaluation.py
+++ titanic_data_evaluation.py
@@ -8,4 +8,10 @@ csv = pd.read_csv("TitanicData.csv")
 csv["Survived"].replace(to_replace=0, value=False, inplace=True)
 csv["Survived"].replace(to_replace=1, value=True, inplace=True)
 survivors = csv.groupby("Survived").agg({"PassengerId": "count"})
-print(survivors)
+
+csv[['Title','Name']] = csv['Name'].str.split('.',1, expand = True)
+csv[['Surname', 'Title']] = csv['Title'].str.split(',',1,expand = True)
+
+title = csv.groupby('Title').describe()
+print(csv[['Title','Name']])
+print(title)

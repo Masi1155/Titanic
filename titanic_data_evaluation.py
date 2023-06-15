487e62c0fc4d99deb405e195e720baea18b186ba
masi <masi.kun@live.de>
2023-06-15 11:33:54 +0200

    added Comments and Cleaned up

--- titanic_data_evaluation.py
+++ titanic_data_evaluation.py
@@ -8,24 +8,20 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import seaborn.objects as so
 
-
-#importing the Data
+# importing the Data
 csv = pd.read_csv("TitanicData.csv")
 csv["Survived"].replace(to_replace=0, value=False, inplace=True)
 csv["Survived"].replace(to_replace=1, value=True, inplace=True)
 survivors = csv.groupby("Survived").agg({"PassengerId": "count"})
 
-#Splitting Name in Title, Surname and Name
+# Splitting Name in Title, Surname and Name
 csv['Title'] = csv['Name'].str.split('.', expand=True)[0].str.split(',', expand=True)[1]
 csv['Surname'] = csv['Name'].str.split('.', expand=True)[0].str.split(',', expand=True)[0]
 csv['Name'] = csv['Name'].str.split('.', expand=True)[1]
 
-
-
 titleSurvived = csv[['Title', 'Survived', 'Pclass']].groupby('Title').apply(lambda x: (x['Survived']).sum())
 pclass_title = csv[['Title', 'Pclass', 'Survived']].groupby(['Pclass', 'Title']).count()
 titleSurvived = csv.groupby('Title').agg({'Survived': 'count', 'Pclass': 'count'})
-
 
 fig, axes = plt.subplots(nrows=3, ncols=2, squeeze=False, figsize=(18, 10))
 plt.subplots_adjust(wspace=0.2, hspace=0.8)
@@ -44,6 +40,5 @@ survive_fare_survived.plot(style=".", ax=axes[1, 1], legend=False, xlim=(-10, 52
 axes[1, 1].set_title("Überlebt")
 survive_fare_died.plot(style=".", ax=axes[1, 0], legend=False, color="c", xlim=(-10, 520), ylim=(-1, 30))
 axes[1, 0].set_title("Gestorben")
-
 
 plt.show()
